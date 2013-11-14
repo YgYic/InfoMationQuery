cbc0204e491e79acde92266b1d3a0b007918d928
ygyic <269241127@qq.com>
2013-11-14 10:21:14 +0800

    1

--- InformationQuery/InformationQuery/src/junit/TestMain.java
+++ InformationQuery/InformationQuery/src/junit/TestMain.java
@@ -1,15 +1,22 @@
 package junit;
 
 
-import static org.junit.Assert.*;
+import java.util.Random;
 
 import org.junit.Test;
 
 public class TestMain {
 	@Test
 	public void testMain(){
-		System.out.println("Junit");
-		assertTrue(true);
+		Random fromIndex = new Random(11);
+		for (int i = 0; i < 300; i++) {
+			if (fromIndex.nextInt(11)<0) {
+				System.out.println("cuowu 1");
+			}
+			if (fromIndex.nextInt(11)>10) {
+				System.out.println("cuowu 2");
+			}
+		}
 	}
 
 }
